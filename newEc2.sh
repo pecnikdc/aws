047ed57bc0add8a4655785d128a127dab519015a
pecnikdc <pecnikdc@gmail.com>
2017-09-10 01:25:01 -0700

    Update newEc2.sh

--- newEc2.sh
+++ newEc2.sh
@@ -6,4 +6,4 @@ tar -xzvf teleport-v2.2.7-linux-amd64-bin.tar.gz
 cd teleport
 sudo make install
 
-sudo ./teleport start --roles=node --auth-server=login.gus.com:3025 --token=AFjTQd9v3B39831h82wFbr4z2zFReQ043A4 --nodename=pre-k &
+sudo ./teleport start --roles=node --auth-server=login.gus.com:3025 --token=AFjTQd9v3B39831h82wFbr4z2zFReQ043A4 --nodename=$(hostname) &
