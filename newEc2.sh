d992a7b1c396c0bcaf7c223d93854cd1aeeef1c1
pecnikdc <pecnikdc@gmail.com>
2017-09-10 01:23:32 -0700

    Update newEc2.sh

--- newEc2.sh
+++ newEc2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd ~
-wget https://github.com/gravitational/teleport/releases/download/v2.3.0-rc2/teleport-v2.3.0-rc2-linux-amd64-bin.tar.gz
-tar -xzvf teleport-v2.3.0-rc2-linux-amd64-bin.tar.gz
+wget https://github.com/gravitational/teleport/releases/download/v2.2.7/teleport-v2.2.7-linux-amd64-bin.tar.gz
+tar -xzvf teleport-v2.2.7-linux-amd64-bin.tar.gz
 
 cd teleport
 sudo make install
 
-teleport start --roles=node --token= --auth-server=
+sudo ./teleport start --roles=node --auth-server=login.gus.com:3025 --token=AFjTQd9v3B39831h82wFbr4z2zFReQ043A4 --nodename=pre-k &
